bdef670abfc5ffb9307208aac3c2e2482e39e3ca
Alima-T <okhapka.a@gmail.com>
2025-01-01 19:38:29 +0100

    edit header file

--- philos.h
+++ philos.h
@@ -6,7 +6,7 @@
 /*   By: alima <alima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 19:26:38 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/01/01 18:00:43 by alima            ###   ########.fr       */
+/*   Updated: 2025/01/01 18:50:58 by alima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,19 +59,27 @@ typedef struct s_philo
 
 /* Функции инициализации (init.c) */
 int			init_av_data(t_data *data, int ac, char **av);
-int			init_mutexes(t_data *data);
 void		init_philos(t_philo *philos, t_data *data, pthread_mutex_t *fork);
+void		destroy_mutex(t_philo *philos, t_data *data);
+int			run_threads(t_philo *philos, t_data *data);
+int 		start_sim(t_data *data);
 
 /* Вспомогательные функции (utils.c) */
 long long	get_time(void);              /* Получение текущего времени */
-void		smart_sleep(long long time);  /* Умная задержка */
-void		print_status(t_philo *philo, char *status); /* Печать статуса */
+int			error_msg(int err_num);
+void		philos_msg(int msg_code, long time, int id, pthread_mutex_t *print_mutex);
+int			ft_atoi(const char *str);
 int			check_death(t_philo *philo);  /* Проверка смерти */
 void		free_all(t_data *data, t_philo *philos); /* Освобождение памяти */
 
 /* Функции действий философов (actions.c) */
-void		*philo_routine(void *arg);    /* Основной цикл философа */
-void		eat(t_philo *philo);          /* Прием пищи */
-void		sleep_think(t_philo *philo);  /* Сон и размышления */
+void		eat_or_sleep(long time);
+void		eat(t_philo *philos);
+void		sleep_think(t_philo *philo); 
+void		take_forks(t_philo *philos);
+void		*philo_routine(void *arg);  /* Основной цикл философа */
+/* philo.c */
+static int	ft_check_valid(int ac, char **av);
+int			main(int ac, char **av);
 
 #endif
